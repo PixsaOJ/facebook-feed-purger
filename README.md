33ed0650584e2ba88191348dfee2df11c8ebbd54
Pixsa <sabasaba101010@gmail.com>
2020-07-21 00:20:16 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Facebook Feed Purger
+
+## What it does
+
+It removes news feed from new version of Facebook.com.
+
+
+## How to use it
+
+* Change into the example directory and run `npm install` to install all
+  dependencies.
+* Execute `npm run lint` to view a report of any coding errors.
---
+++ background.js
@@ -0,0 +1,68 @@
+const CSS = "div[role='feed'], div[data-pagelet='MainFeed'] { display: none }";
+const TITLE_APPLY = "Apply CSS";
+const TITLE_REMOVE = "Remove CSS";
+const APPLICABLE_DOMAIN = "facebook.com";
+
+/*
+Toggle CSS: based on the current title, insert or remove the CSS.
+Update the page action's title and icon to reflect its state.
+*/
+function toggleCSS(tab) {
+
+
+  function gotTitle(title) {
+    if (title === TITLE_APPLY) {
+      browser.pageAction.setIcon({tabId: tab.id, path: "icons/on.svg"});
+      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
+      browser.tabs.insertCSS({code: CSS});
+    } else {
+      browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
+      browser.pageAction.setTitle({tabId: tab.id, title: TITLE_APPLY});
+      browser.tabs.removeCSS({code: CSS});
+    }
+  }
+
+  var gettingTitle = browser.pageAction.getTitle({tabId: tab.id});
+  gettingTitle.then(gotTitle);
+}
+
+/*
+Returns true only if the URL's domain is APPLICABLE_DOMAIN.
+*/
+function protocolIsApplicable(url) {
+  return url.includes(APPLICABLE_DOMAIN);
+}
+
+/*
+Initialize the page action: set icon and title, then show.
+Only operates on tabs whose URL's domain is applicable.
+*/
+function initializePageAction(tab) {
+  if (protocolIsApplicable(tab.url)) {
+    browser.pageAction.setIcon({tabId: tab.id, path: "icons/off.svg"});
+    browser.pageAction.setTitle({tabId: tab.id, title: TITLE_REMOVE});
+    browser.pageAction.show(tab.id);
+  }
+}
+
+/*
+When first loaded, initialize the page action for all tabs.
+*/
+var gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+  for (let tab of tabs) {
+    initializePageAction(tab);
+  }
+});
+
+/*
+Each time a tab is updated, reset the page action for that tab.
+*/
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+  initializePageAction(tab);
+});
+
+/*
+Toggle CSS when the page action is clicked.
+*/
+browser.pageAction.onClicked.addListener(toggleCSS);
